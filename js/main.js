6879a12932537bd94dcc3a8ca7f8ec3a6a28f4d6
MarcosZacarias <marcoszacariasdaniel@gmail.com>
2023-08-21 17:19:22 +0200

    add: 3 types grid

--- js/main.js
+++ js/main.js
@@ -2,12 +2,23 @@
 
 const cellContainer = document.getElementById("cell-container");
 const playButton = document.getElementById("play-button");
+const difficulty = document.getElementById("inputGroupSelect04");
+let difficultySelect = difficulty.value;
+console.log(difficultySelect);
+
+let cellTotal;
 
 // |Creazione numero di celle
-const cellTotal = 10 * 10;
+if (difficultySelect == 1) {
+  cellTotal = 7 * 7;
+} else if (difficultySelect == 2) {
+  cellTotal = 9 * 9;
+} else if (difficultySelect == 3) {
+  cellTotal = 10 * 10;
+}
 
 // |Generazione griglia
-createGrid(cellTotal, cellContainer);
+// createGrid(cellTotal, cellContainer);
 
 // |Pulsante ceazione griglia
 playButton.addEventListener("click", function () {
@@ -18,8 +29,16 @@ playButton.addEventListener("click", function () {
 
 function createCell(container, i) {
   const cell = document.createElement("li");
+
   cell.innerText = i;
   cell.classList.add("cell");
+  if (difficultySelect == 1) {
+    cell.classList.add("cell7");
+  } else if (difficultySelect == 2) {
+    cell.classList.add("cell9");
+  } else {
+    cell.classList.add("cell10");
+  }
   cell.addEventListener("click", function () {
     cell.classList.add("bg-primary");
     console.log(i);
@@ -32,7 +51,7 @@ function createCell(container, i) {
 function createGrid(cellsNumber, container) {
   container.innerHTML = "";
   // |Ciclo creazioni celle
-  for (let i = 1; i <= cellTotal; i++) {
+  for (let i = 1; i <= cellsNumber; i++) {
     createCell(container, i);
   }
 }
